3d89f6b24be47a17c225082c6664a7a563697769
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-07 20:42:09 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,12 @@
 /**
  * main-entry path
  *
- * Return : 1
+ * Return : Always 1 (success)
  *
  */
-
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";	
+	char ch[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";	
 	write(2, ch, sizeof(ch));	
 	return (1);
 }
