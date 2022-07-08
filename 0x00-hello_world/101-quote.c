2614ec9d4e784a7b4d8e01888f8b8658e817d014
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-08 00:29:30 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/**
- * main-entry path
+/*
+ *main-entry path
  *
- * Return : Always 1 (success)
+ *Return : Always 1 (success)
  *
  */
 int main(void)
 {
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
 	write(2, ch, sizeof(ch));	
 	return (1);
 }
