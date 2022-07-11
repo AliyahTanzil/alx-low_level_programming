5aaa4a0f4238f787f669ebea84ded94d81851109
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-11 11:44:32 -0700

    Rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * rev_string - this function reverse a string followed by
+ * a new line.
+ * @s: is an input string
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char mas;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		mas = s[i];
+		s[i++] = s[len];
+		s[len] = mas;
+	}
