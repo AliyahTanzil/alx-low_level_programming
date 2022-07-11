9fa8760568f0c2522b21117ba2babf6adbb56c3c
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-10 21:15:17 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - Entry  point
+ *
  * Return:1
  */
 int main(void)
@@ -9,5 +10,4 @@ int main(void)
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, ch, sizeof(ch));
 	return (1);
-
 }
