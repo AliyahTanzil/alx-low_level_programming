dcd45bd3f4030e38bbfdd22ae715f4f626602cdc
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-12 10:38:58 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_alphabet - view description
+ * Description: Prints the alphabet in lowercase followed by a new line
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
