0c20a8039f86abd3abceeb41b5dd770fe35017ae
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-12 10:07:03 -0700

    _Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
 #include <unist.h>
+#include "holberton.h"
 
 /**
- *
- * 
+ * main - check description
+ * Description: it prints the word _putchar, followed by new line.
+ * Return: 0.
  */
 
-int _putchar(char c)
+int main(void)
 {
-	return(write(1, &c, 1));
+	char word[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
 }
