298a5a886f46baa99474c22a00ee9cae8d64d2c7
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-13 14:40:29 -0700

    All

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcat - is a function that concatenates two strings.
+ * @dest: input string
+ * @src: input string
+ * Return: A pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (temp);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _srtncat - is a function which concatenates two strings
+ * @dest: input string
+ * @src: input string
+ * @n: input integer
+ * Return: A pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	while (*dest)
+		dest++;
+
+	if ( n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
