5d6f017cc903ab429edbf686d263f2c0c07d092a
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-14 10:26:28 -0700

    Copy Strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - is a function that copys the strings.
+ * @dest: an input strings
+ * @src: input strings
+ * @n: input integer
+ * Return: A pointer that resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	srclen++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (temp);
+}
