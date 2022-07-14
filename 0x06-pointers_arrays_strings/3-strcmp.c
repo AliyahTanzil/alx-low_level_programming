fe7082b824e50535d07132331d4a2ed448437876
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-14 10:45:57 -0700

    Substraction

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - is a function that compares two strings.
+ * @s1: input string
+ * @s2: input string
+ * Return: The diffrence s1 - s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++; s2++;
+	}
+	return (*s1 - *s2);
+}
