b752c36828a459a6465f19085010020e0df3ba73
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-15 16:17:38 -0700

    Alphabet

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,12 +1,10 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
  * main - checker of code
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
-
 int main(void)
 {
 	print_alphabet_x10();
