ae175695529f19be15db5288f90088f381cf80ea
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-14 21:31:09 -0700

    Reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - is a function that reverse an array.
+ * @a: input array
+ * @n: number of elements of array
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int aux;
+
+	while (i < n--)
+	{
+		aux = a[i];
+		a[i++] = a[n];
+		a[n] = aux;
+	}
+}
