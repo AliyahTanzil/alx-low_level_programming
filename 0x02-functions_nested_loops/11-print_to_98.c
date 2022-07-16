8fc7c01213d1ca0e9203442b2c3f31b6f4b4636a
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-15 23:32:06 -0700

    11 to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - checker code
+ * @n: input integer
+ * Descritpion: function that print
+ * from n to 98, followed by new line.
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d\n", n);
+
+			if (n == 98)
+				continue;
+			printf(",");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d\n", n);
+
+			if (n == 98)
+				continue;
+			printf(",");
+		}
+		printf("\n");
+	}
+}
