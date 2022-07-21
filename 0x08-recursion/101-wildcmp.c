148153eefc1d899c9fd4d5e3b1cfcb05c4bb31f8
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-21 01:04:30 -0700

    Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,69 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * wildcmp - check the code wild charcater
- * @s: string
- * @s1: string
- * Return: Always 0.
+ * wildcmp - is a function that compares two strings
+ * @s: first input
+ * @s1: second input
+ * Return: 1 if strings identical and 0 in otherwise
  */
-
 int wildcmp(char *s, char *s1)
 {
-	return (checker(s, s1, 0, 0, -1));
-}
-
-/**
- * checkLast - check for the last char of the string
- * @s: string
- * @i: int
- * Return: 0 or 1
- */
-int checkLast(char *s, int i)
-{
-	if (s[i] == '*')
-		return (checkLast(s, i + 1));
-	else if (s[i] == '\0')
+	if (!*s  && !*s1)
 		return (1);
-
+	if (*s == *s1)
+		return (wildcmp(s + 1, s1 + 1));
+	if (*s1 == '*' && (wildcmp(s, s1 + 1) || wildcmp(s + 1, s1)))
+		return (1);
+	if (*s1 == '*' && *(s + 1) && *s1)
+		return (0);
 	return (0);
-}
-/**
- * checker - helper
- * @s: string
- * @s1: string
- * @a: int
- * @b: int
- * @wildUsed: int
- * Return: Always 0.
- */
-int checker(char *s, char *s1, int a, int b, int wildUsed)
-{
-	if (s[a] != '\0')
-	{
-		if (s1[b] == '\0')
-			return (0);
-		else if (s1[b] == '*')
-		{
-			if (s1[b + 1] == '*')
-				return (checker(s, s1, a, b + 1, b));
-			else if (s1[b + 1] == s[a])
-				return (checker(s, s1, a, b + 1, b));
-			else if (s[a + 1] != s1[b + 1])
-				return (checker(s, s1, a + 1, b, b));
-			else if (s[a + 1] == s1[b + 1])
-				return (checker(s, s1, a + 1, b + 1, b));
-		}
-		else if ((s[a] == s1[b]) || (s1[b] == '*' && s1[b + 1] == s[a + 1]))
-			return (checker(s, s1, a + 1, b + 1, wildUsed));
-
-		if (wildUsed == -1)
-			return (0);
-
-		return (checker(s, s1, a, wildUsed, wildUsed));
-	}
-	if (s1[b] != '\0')
-		return (checkLast(s1, b));
-
-	return (1);
 }
