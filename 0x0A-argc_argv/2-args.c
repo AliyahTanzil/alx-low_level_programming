3cb923b6a8655b99e7c07fe8f7e3ed5954c9ec08
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-22 12:26:26 -0700

    Print number of arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: parameter
+ * @argv: an array of a command
+ * Return: Alwayas 0
+ */
+int main(int argc, char *argv[])
+{
+	int  i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv index %d is = %s\n", i, argv[i]);
+	}
+	return (0);
+}
