ef96bb91ca46561a5adaf58e78339111f8595b9b
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-22 12:16:51 -0700

    argc use to indentify the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: parameter
+ * @argv: An array of a command listed
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
