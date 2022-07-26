fc4ac3e09a5207914c3af1e9dae7c8c9e18d1168
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-26 08:43:43 -0700

    Static Libraries

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates @src to @dest
+ *_strcat - concatenates @src to @dest
  * @src: the source string to append to @dest
  * @dest: the destiation string to be concatenated upon
  * Return:pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0;
-	int dest_len = 0;
+	int c, c1;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (c1 = 0; src[c1]; c1++)
+		dest[c++] = src[c1];
 	return (dest);
 }
