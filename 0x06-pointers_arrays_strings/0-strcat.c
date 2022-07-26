467055b98cc365070e86edc53212f68a7b2b09ba
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-26 06:59:37 -0700

    Pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,18 +6,16 @@
  * @dest: the destiation string to be concatenated upon
  * Return:pointer to the resulting string
  */
-
 char *_strcat(char *dest, char *src)
-
 {
+	int c, c1;
 
-int index = 0;
-int dest_len = 0;
+	c = 0;
 
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-return (dest);
+	while (dest[c])
+		c++;
 
+	for (c1 = 0; src[c1]; c1++)
+		dest[c++] = src[c1];
+	return (dest);
 }
