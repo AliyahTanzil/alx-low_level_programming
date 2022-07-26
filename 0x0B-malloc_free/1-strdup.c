cd2dc023dc77a40e889d6a184c5f71b4be201a5b
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-26 11:08:22 -0700

    Strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer
+ * @str: The string to be copied
+ * Retiurn: str == NULL or insufficient meemory
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	int index, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (index = 0; str[index]; index++)
+		len++;
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = '\0';
+	return (duplicate);
+}
