751a3f16dbbb35c13373097d12dc31eb1364a1d7
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-27 03:22:29 -0700

    String

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,25 +9,45 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char  *concat_str;
-	int index, concat_index = 0, len = 0;
+	char *new_str, *starts1, *starts2;
+	int i = 0, lens1 = 0, lens2 = 0;
 
+	starts1 = s1;
+	starts2 = s2;
 	if (s1 == NULL)
 		s1 = " ";
+	while (*s1)
+	{
+		lens1++;
+		s1++;
+	}
+	s1 = starts1;
 	if (s2 == NULL)
 		s2 = " ";
-
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-	concat_str = malloc(sizeof(char) * len);
-
-	if (concat_str == NULL)
+	while (*s2)
+	{
+		lens2++;
+		s2++;
+	}
+	s2 = starts2;
+	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	starts1 = new_str;
+	if (new_str == NULL)
 		return (NULL);
-
-	for (index = 0; s2[index++]; index++)
-		concat_str[concat_index++] = s1[index];
-
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
-	return (concat_str);
+	for (; i < (lens1 + lens2); i++)
+	{
+		if (i < lens1)
+		{
+			new_str[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[i] = *s2;
+			s2++;
+		}
+	}
+	new_str[i] = '\0';
+	return (starts1);
 }
+
