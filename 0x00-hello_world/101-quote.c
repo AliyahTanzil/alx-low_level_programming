641e95ad95944967e7527ec16a809e0b910b3de2
Mohamed Augustin Sesay <sesaymohamedaugustin@gmail.com>
2022-07-28 00:56:39 +0000

    Log

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
 /**
- * main - Entry  point
+ * main - prints the standrad error sandwiched around the  standard outpu/t
  *
- * Return:1
+ * Return: returns 1
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, ch, sizeof(ch));
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-
 }
