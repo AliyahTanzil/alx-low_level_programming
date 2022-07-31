e9df5a217ce44011d814a724256a9149f8c708ec
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-07-31 04:41:49 +0000

    Reuse

---
+++ 0x04-more_functions_nested_loops/3-prints_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * prints_numbers - a function that prints numbers
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= '9'; i++)
+		_putchar('\n');
+}
