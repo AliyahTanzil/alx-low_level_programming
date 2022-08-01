7441c08c9494d3968baaa6a58f94adaef6637600
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-08-01 10:04:51 -0700

    Structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * new_dog - creates a new dog structure@name: Dog's name
+ * new_dog - creates a new dog structure
+ * @name: Dog's name
  * @age: Dog's age
  * @owner: Dog's owner
  * Return: returns a pointer to newly created dog structure
@@ -12,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	char *cpyname, *cpyowner;
 	int len_name = 0, len_owner = 0, i;
-	
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	while (name[len_name])
