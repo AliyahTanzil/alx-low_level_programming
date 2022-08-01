23f07b6f55e07451422aa6a6154010f46b57d3ef
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-08-01 12:38:59 +0000

    Object 2

--- 0x0D-preprocessor/1-main.c
+++ 0x0D-preprocessor/1-main.c
@@ -1,15 +1,19 @@
+#include "1-pi.h"
+#include "1-pi.h"
 #include <stdio.h>
-#include "dog.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	struct dog my_dog;
-	init_dog(&my_dog, "Django", 3.5, "Jay");
-	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	float a;
+	float r;
+
+	r = 98;
+	a = PI * r * r;
+	printf("%.3f\n", a);
 	return (0);
 }
